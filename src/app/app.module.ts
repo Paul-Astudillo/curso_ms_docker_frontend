a9397de88a1aca0bdb8b2e08a0815f13997b8f4b
Diego <jtapiav4@est.ups.edu.ec>
2023-12-20 12:08:18 -0500

    cambio directorio template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { RegistrarprofesorComponent } from './pages/registrarprofesor/registrarp
 import { RegistrarcursoComponent } from './pages/registrarcurso/registrarcurso.component';
 import { ListarprofesorComponent } from './pages/listarprofesor/listarprofesor.component';
 import { ListarcursoComponent } from './pages/listarcurso/listarcurso.component';
-import { TituloComponent } from './templete/titulo/titulo.component';
-import { MenuComponent } from './templete/menu/menu.component';
+import { TituloComponent } from './template/titulo/titulo.component';
+import { MenuComponent } from './template/menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './pages/inicio/inicio.component';
 
